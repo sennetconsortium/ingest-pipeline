00c9ac96d113b50e84f2a66865bea2db68892bf6
David Betancur <dbetancur@psc.edu>
2023-04-18 12:39:10 -0400

    POC getting the environment instance to spawn the correct image

--- src/ingest-pipeline/airflow/dags/validate_upload.py
+++ src/ingest-pipeline/airflow/dags/validate_upload.py
@@ -26,6 +26,7 @@ from utils import (
     get_preserve_scratch_resource,
     get_threads_resource,
     get_instance_type,
+    get_environment_instance
     )
 
 from aws_utils import (
@@ -70,7 +71,8 @@ with HMDAG("validate_upload",
 
     def start_new_environment(**kwargs):
         uuid = kwargs["dag_run"].conf["submission_id"]
-        instance_id = create_instance(uuid, "Airflow Worker", get_instance_type(kwargs.get("dag_id")))
+        instance_id = create_instance(uuid, f"Airflow {get_environment_instance()} Worker",
+                                      get_instance_type(kwargs["dag_run"].conf["dag_id"]))
         if instance_id is None:
             return 1
         else:
