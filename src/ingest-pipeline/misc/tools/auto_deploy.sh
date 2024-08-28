f92d291c7d52fce932ce530dbf36c0b582a5e26a
David Betancur <dbetancur@psc.edu>
2024-08-28 12:48:59 -0400

    Updating auto_deploy for moonshot

--- src/ingest-pipeline/misc/tools/auto_deploy.sh
+++ src/ingest-pipeline/misc/tools/auto_deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # What variables do we need?
-# Define HIVE machines
-#hive_machines=("l001.hive.psc.edu" "gpu002.pvt.hive.psc.edu")
-hive_machines=()
-#b2_machines=("v004.pvt.bridges2.psc.edu")
-b2_machines=()
+# Define CODCC machines
+#codcc_machines=("c0" "c1" "c2" "c3" "c4" "c5")
+codcc_machines=()
 repo_env="dev"
 repo_dir="/opt/repositories/$(hostname -s)-$repo_env/ingest-pipeline"
+python_version=3.9
 
 regenerate_env=false
 
@@ -23,23 +22,12 @@ while getopts ":g" opt; do
 done
 
 set -x
-for machine in ${hive_machines[@]}; do
+for machine in "${codcc_machines[@]}"; do
        	# Rsync repo to machine
         rsync -a --exclude "src/ingest-pipeline/airflow/logs" $repo_dir/ $machine:$repo_dir
 
        	# If flag set, run the conda environment regenerations
         if $regenerate_env ; then
-                ssh $machine "/usr/local/bin/update_hubmap.sh $repo_dir $repo_env"
-        fi
-done
-
-# Separate because its easier to loop twice over a small list than insert string checking and manipulation
-for machine in ${b2_machines[@]}; do
-        # Rsync repo to machine
-        rsync -a --exclude "src/ingest-pipeline/airflow/logs" 'ssh -J bridges2.psc.edu' $repo_dir/ $machine:$repo_dir
-
-        # If flag set, run the conda environment regenerations
-        if $regenerate_env ; then
-                ssh -J "bridges2.psc.edu" $machine "/usr/local/bin/update_hubmap.sh $repo_dir $repo_env"
+                ssh $machine "/usr/local/bin/update_hubmap.sh $repo_dir $repo_env $python_version"
         fi
 done
