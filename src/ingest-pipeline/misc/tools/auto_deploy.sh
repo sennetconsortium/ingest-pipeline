94fb7888def5af0dfb2942abc1e46ab96f934a90
David Betancur <dbetancur@psc.edu>
2024-08-29 14:06:16 -0400

    Updating auto_deploy for moonshot to support env variables from sysconfig files.

--- src/ingest-pipeline/misc/tools/auto_deploy.sh
+++ src/ingest-pipeline/misc/tools/auto_deploy.sh
@@ -2,13 +2,28 @@
 
 # What variables do we need?
 # Define CODCC machines
-#codcc_machines=("c0" "c1" "c2" "c3" "c4" "c5")
+# codcc_machines=("c0" "c1" "c2" "c3" "c4" "c5")
 codcc_machines=()
-repo_env="dev"
-repo_dir="/opt/repositories/$(hostname -s)-$repo_env/ingest-pipeline"
-python_version=3.9
+
+priority_list=("-prod -test -dev")
+base_name="airflow"
+file_dir="/etc/sysconfig"
 
 regenerate_env=false
+
+function set_main_environment(){
+  for ending in "${priority_list[@]}"; do
+    file="$file_dir/$base_name$ending"
+    if [ -f "$file" ]; then
+        echo "Selected file: $file"
+        source $file
+        repo_env="$HUBMAP_INSTANCE"
+        python_version="$HUBMAP_PYTHON_VERSION"
+        repo_dir="/opt/repositories/$(hostname -s)-$repo_env/ingest-pipeline"
+        break
+    fi
+done
+}
 
 while getopts ":g" opt; do
   case $opt in
@@ -22,6 +37,13 @@ while getopts ":g" opt; do
 done
 
 set -x
+
+set_main_environment
+
+if [ -z "$repo_env" ]; then
+    exit "The environment variable HUBMAP_INSTANCE is not set."
+fi
+
 for machine in "${codcc_machines[@]}"; do
        	# Rsync repo to machine
         rsync -a --exclude "src/ingest-pipeline/airflow/logs" $repo_dir/ $machine:$repo_dir
